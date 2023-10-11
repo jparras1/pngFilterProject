641c8f402f16eb907283c7ed771c5e611c1c18a1
Jonathan Parras <jparras@email.com>
2023-10-11 11:27:15 -0700

    created unzipped function

--- IOhandler.js
+++ IOhandler.js
@@ -3,8 +3,8 @@
  * File Name: IOhandler.js
  * Description: Collection of functions for files input/output related operations
  *
- * Created Date:
- * Author:
+ * Created Date: October 11, 2023
+ * Author: Jonathan Parras
  *
  */
 
@@ -12,6 +12,7 @@ const unzipper = require("unzipper"),
   fs = require("fs"),
   PNG = require("pngjs").PNG,
   path = require("path");
+const { pipeline, Transform } = require("stream");
 
 /**
  * Description: decompress file from given pathIn, write to given pathOut
@@ -20,7 +21,21 @@ const unzipper = require("unzipper"),
  * @param {string} pathOut
  * @return {promise}
  */
-const unzip = (pathIn, pathOut) => {};
+const unzip = (pathIn, pathOut) => {
+  return new Promise( (resolve, reject) => {
+    pipeline (
+      fs.createReadStream(pathIn),
+      unzipper.Extract({path: pathOut}),
+
+      function onEnd(err) {
+        if (err) {
+          reject(`Error: ${err}`);
+        }
+      },
+      resolve()
+    );
+  });
+};
 
 /**
  * Description: read all the png files from given directory and return Promise containing array of each png file path
